3f95c7c3449bd4d951ea49eba80c714726ff6de2
Preeth02 <preethampreetham27@gmail.com>
2024-12-03 18:17:44 +0530

    add user controllers, routes and user middleware

--- LBS BE/src/app.js
+++ LBS BE/src/app.js
@@ -3,10 +3,14 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
 
+//Controllers
+import userRoutes from "./routes/user.routes.js";
+
 app.use(cors({ credentials: true, origin: process.env.CORS_ORIGIN }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static("public"));
 
+app.use("/api/v1/user", userRoutes);
 export default app;
---
+++ LBS BE/src/middlewares/verifyUserJWT.middleware.js
@@ -0,0 +1,55 @@
+import asyncHandler from "../utils/asyncHandler.js";
+import apiError from "../utils/apiError.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.models.js";
+
+const verifyAnyJwtFunc = (token, secret) => {
+  try {
+    return jwt.verify(token, secret);
+  } catch (error) {
+    return { error };
+  }
+};
+
+export const verifyUserJWT = asyncHandler(async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      throw new apiError(401, "Unothrized request");
+    }
+
+    //Trying to verify the user's token first and if there's an error storing it in a variable
+    const userDecodedToken = verifyAnyJwtFunc(
+      token,
+      process.env.ACCESS_TOKEN_SECRET
+    );
+    //Verifying the admin's token and returning error
+    const adminDecodedToken = verifyAnyJwtFunc(
+      token,
+      process.env.ACCESS_TOKEN_ADMIN_SECRET
+    );
+    console.log({ userDecodedToken, adminDecodedToken });
+
+    if (userDecodedToken?.error && adminDecodedToken?.error) {
+      console.log({ userTokenError, adminTokenError });
+      throw new apiError(400, "Invalid token");
+    }
+    const decodedToken = !userDecodedToken?.error
+      ? userDecodedToken
+      : adminDecodedToken;
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+    if (!user) {
+      throw new apiError(401, "Invalid access token");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new apiError(401, error?.message || "Invalid access token");
+  }
+});
